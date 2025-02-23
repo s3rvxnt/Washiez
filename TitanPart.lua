bc73246cf58b2c59f33134e0f6ace7575be72dfb
s3rvxnt <119671082+s3rvxnt@users.noreply.github.com>
2025-02-22 18:56:12 -0600

    Update TitanPart.lua

--- TitanPart.lua
+++ TitanPart.lua
@@ -4,13 +4,13 @@ local Player = game:GetService("Players").LocalPlayer
 Player.PlayerScripts:ClearAllChildren()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/s3rvxnt/Washiez/refs/heads/main/Utils"))()
 local LimbOffsets = {
-    Head = CFrame.Angles(math.rad(-90),0,math.rad(180)) * CFrame.new(0,-4.5,-7),
-    TorsoLeft = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(-5,-3,-3),
-    TorsoRight = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(5,-3,-3),
-    RightArm = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(0,-3,-3),
-    LeftArm = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(0,-3,-3),
-    RightLeg = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(0,-3,-1.5),
-    LeftLeg = CFrame.Angles(math.rad(-90),0,0) * CFrame.new(0,-3,-1.5)
+    Head = CFrame.Angles(math.rad(-90), 0, math.rad(180)) * CFrame.new(0, -4.5, -7),
+    TorsoLeft = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(-5, -3, -3),
+    TorsoRight = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(5, -3, -3),
+    RightArm = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(0, -3, -3),
+    LeftArm = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(0, -3, -3),
+    RightLeg = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(0, -3, -1.5),
+    LeftLeg = CFrame.Angles(math.rad(-90), 0, 0) * CFrame.new(0, -3, -1.5)
 }
 local _, dimensions =
     game:GetService("ReplicatedStorage"):WaitForChild("CarSpawnSystem"):WaitForChild("CarModels"):WaitForChild("Van"):GetBoundingBox(
@@ -19,139 +19,159 @@ local _, dimensions =
 local volumethreshold = dimensions.X * dimensions.Y * dimensions.Z
 local function GetVan()
     local OriginalVehicle = getgenv().WashiezGetVehicle()
-    local Vehiclea = OriginalVehicle
+    local Character = Player.Character or Player.CharacterAdded:Wait()
+    Character:WaitForChild("Humanoid").Sit = false
+    task.wait(Player:GetNetworkPing())
+    for _, v in Character:GetChildren() do
+        pcall(
+            function()
+                v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+                v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+            end
+        )
+    end
+    Character:PivotTo(CFrame.new(0, 1000000, 0))
+    getsenv(game:GetService("Players").LocalPlayer.PlayerGui.CarSelection.Manager).closeMenu()
     repeat
         task.wait()
-        pcall(function()
-        game:GetService("Players").LocalPlayer.Character:PivotTo(CFrame.new(0,100000,0))
-        local SCRIPT = game:GetService("Players").LocalPlayer.PlayerGui.CarSelection.Manager
-        local environment = getsenv(SCRIPT)
-        environment.closeMenu()
-        task.wait(1)
-        game:GetService("Players").LocalPlayer.Character.Humanoid.Sit = false
-        task.wait(game:GetService("Players").LocalPlayer:GetNetworkPing())
-        game:GetService("Players").LocalPlayer.Character:PivotTo(CFrame.new(0,100000,0))
-        task.wait(2)
-        getgenv().WashiezRequestVehicleSpawn()
-        game:GetService("Players").LocalPlayer.Character:PivotTo(CFrame.new(0,100000,0))
+    until game:GetService("Players").LocalPlayer.PlayerGui.CarSelection.MainFrame.Position ==
+        UDim2.new(1.20000005, 30, 0.5, 0)
+    getgenv().WashiezRequestVehicleSpawn()
+    repeat
+        task.wait()
         getgenv().WashiezSpawnVehicle("Van")
-        local stop = false
-        task.delay(
-        game:GetService("Players").LocalPlayer:GetNetworkPing() + 2,
-        function()
-            stop = true
+        for _, v in Character:GetChildren() do
+            pcall(
+                function()
+                    v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+                    v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+                end
+            )
         end
+    until getgenv().WashiezGetVehicle() ~= nil and getgenv().WashiezGetVehicle() ~= OriginalVehicle
+    for _, v in Character:GetChildren() do
+        pcall(
+            function()
+                v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+                v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+            end
         )
-        repeat
-            task.wait()
-            Vehiclea = getgenv().WashiezGetVehicle()
-        until Vehiclea ~= OriginalVehicle and Vehiclea ~= nil or stop == true
-        if Vehiclea ~= OriginalVehicle and Vehiclea ~= nil then
-            Vehiclea:WaitForChild("Chassis"):WaitForChild("VehicleSeat"):Sit(game:GetService("Players").LocalPlayer.Character.Humanoid)
-            repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.CarSelection.MainFrame.Position == UDim2.new(1, -5, 0.5, 0)
-            environment.closeMenu()
-        end
-            end)
-    until Vehiclea ~= OriginalVehicle and Vehiclea ~= nil
-    return Vehiclea
+    end
+    getgenv().WashiezGetVehicle():WaitForChild("Chassis"):WaitForChild("VehicleSeat"):Sit(
+        game:GetService("Players").LocalPlayer.Character.Humanoid
+    )
+    return getgenv().WashiezGetVehicle()
 end
 local Vehicle = getgenv().WashiezGetVehicle()
 if Vehicle == nil then
     Vehicle = GetVan()
 end
+game:GetService("Workspace").DescendantAdded:Connect(
+    function(obj)
+        pcall(
+            function()
+                obj.CanCollide = false
+                obj.CollisionGroup = "Default"
+            end
+        )
+    end
+)
 
-game:GetService("Workspace").DescendantAdded:Connect(function(obj)
-    pcall(function()
-            obj.CanCollide = false
-            obj.CollisionGroup = "Default"
-    end)
-end)
+task.spawn(
+    function()
+        if not game:GetService("Workspace"):FindFirstChild("CarTitan") then
+            repeat
+                for _, child in game:GetService("Workspace"):GetChildren() do
+                    pcall(
+                        function()
+                            if game:GetService("Players"):FindFirstChild(child.Name) then
+                                --do nothing
+                            elseif
+                                child.Name ~= "SpawnedCars" and child ~= game:GetService("Workspace").CurrentCamera and
+                                    child.Name ~= "Nametags"
+                             then
+                                child:Destroy()
+                            end
+                        end
+                    )
+                end
+                task.wait(30)
+            until false
+        else
+            repeat
+                for _, d in game:GetService("Workspace").SpawnedCars:GetDescendants() do
+                    pcall(
+                        function()
+                            d.CanCollide = false
+                            d.CollisionGroup = "Default"
+                        end
+                    )
+                end
+                task.wait(5)
+            until false
+        end
+    end
+)
 
-task.spawn(function()
-    if not game:GetService("Workspace"):FindFirstChild("CarTitan") then
-    repeat
-    for _,child in game:GetService("Workspace"):GetChildren() do
-    pcall(function()
-    if game:GetService("Players"):FindFirstChild(child.Name) then
-  --do nothing
-    elseif child.Name ~= "SpawnedCars" and child ~= game:GetService("Workspace").CurrentCamera and child.Name ~= "Nametags" then
-        child:Destroy()
-    end
-    end)
-end
-    task.wait(30)
-    until false
-    else
-    repeat
-        for _,d in game:GetService("Workspace").SpawnedCars:GetDescendants() do
-            pcall(function()
-                d.CanCollide = false
-                d.CollisionGroup = "Default"
-            end)
-        end
-        task.wait(5)
-    until false
-    end
-end)
-repeat task.wait()
---pcall(function()
-local _, dimensions = Vehicle:GetBoundingBox()
-local currentvolume = dimensions.X * dimensions.Y * dimensions.Z
-if currentvolume > volumethreshold * 4.8 or Vehicle.Parent == nil then
-    print("getting new van")
-    Vehicle = GetVan()
-    print("got new van")
-    Vehicle:PivotTo(CFrame.new(0,1000000,0))
-end
-local data = readfile("Titan.lua")
-local data = game:GetService("HttpService"):JSONDecode(data)
-local temp = Mode 
-if string.find(Mode,"Torso") then 
-temp = "Torso"
-end
-Vehicle:PivotTo(CFrame.new(unpack(data[temp]))*LimbOffsets[Mode])
-Vehicle.PrimaryPart.AssemblyAngularVelocity = Vector3.new(0,0,0)
-Vehicle.PrimaryPart.AssemblyLinearVelocity = Vector3.new(0,0,0)
-local highestVelocity = 0
-local Highest = nil
-for _,d in game:GetService("Workspace").SpawnedCars:GetChildren() do
-    local IsFriendly = false
-    for index,name in getgenv().UsernameList do
-        if string.find(d.Name,name) then
-            IsFriendly = true
-        end
-    end
-    if not IsFriendly then
-        if not table.find(getgenv().UsernameList,d.Name) then
-            if d.PrimaryPart.AssemblyLinearVelocity.Magnitude > highestVelocity then
-                highestVelocity = d.PrimaryPart.AssemblyLinearVelocity.Magnitude
-                Highest = d
+repeat
+    task.wait()
+    pcall(
+        function()
+            local _, dimensions = Vehicle:GetBoundingBox()
+            local currentvolume = dimensions.X * dimensions.Y * dimensions.Z
+            if currentvolume > volumethreshold * 4.8 or Vehicle.Parent == nil then
+                Vehicle = GetVan()
             end
-            if d.PrimaryPart.AssemblyAngularVelocity.Magnitude > highestVelocity then 
-                highestVelocity = d.PrimaryPart.AssemblyAngularVelocity.Magnitude
-                Highest = d
+            getgenv().WashiezGetVehicle():WaitForChild("Chassis"):WaitForChild("VehicleSeat"):Sit(
+                game:GetService("Players").LocalPlayer.Character.Humanoid
+            )
+            local data = readfile("Titan.lua")
+            local data = game:GetService("HttpService"):JSONDecode(data)
+            local temp = Mode
+            if string.find(Mode, "Torso") then
+                temp = "Torso"
             end
+            Vehicle:PivotTo(CFrame.new(unpack(data[temp])) * LimbOffsets[Mode])
+            Vehicle.PrimaryPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+            Vehicle.PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+            local highestVelocity = 0
+            local Highest = nil
+            for _, d in game:GetService("Workspace").SpawnedCars:GetChildren() do
+                local IsFriendly = false
+                for index, name in getgenv().UsernameList do
+                    if string.find(d.Name, name) then
+                        IsFriendly = true
+                    end
+                end
+                if not IsFriendly then
+                    if not table.find(getgenv().UsernameList, d.Name) then
+                        if d.PrimaryPart.AssemblyLinearVelocity.Magnitude > highestVelocity then
+                            highestVelocity = d.PrimaryPart.AssemblyLinearVelocity.Magnitude
+                            Highest = d
+                        end
+                        if d.PrimaryPart.AssemblyAngularVelocity.Magnitude > highestVelocity then
+                            highestVelocity = d.PrimaryPart.AssemblyAngularVelocity.Magnitude
+                            Highest = d
+                        end
+                    end
+                end
+            end
+            for _, d in game:GetService("Players"):GetPlayers() do
+                if not table.find(getgenv().UsernameList, d.Name) then
+                    if d.Character then
+                        if d.Character.PrimaryPart.AssemblyLinearVelocity.Magnitude > highestVelocity then
+                            highestVelocity = d.Character.PrimaryPart.AssemblyLinearVelocity.Magnitude
+                            Highest = d
+                        end
+                        if d.Character.PrimaryPart.AssemblyAngularVelocity.Magnitude > highestVelocity then
+                            highestVelocity = d.Character.PrimaryPart.AssemblyAngularVelocity.Magnitude
+                            Highest = d
+                        end
+                    end
+                end
+            end
+            local FinalVelocity = math.clamp(highestVelocity, 0, 1500)
+            Vehicle.PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, (FinalVelocity * -1) - 200, 0)
         end
-    end
-end
-for _,d in game:GetService("Players"):GetPlayers() do
-    if not table.find(getgenv().UsernameList,d.Name) then
-        if d.Character then
-            if d.Character.PrimaryPart.AssemblyLinearVelocity.Magnitude > highestVelocity then
-                highestVelocity = d.Character.PrimaryPart.AssemblyLinearVelocity.Magnitude
-                Highest = d
-            end
-            if d.Character.PrimaryPart.AssemblyAngularVelocity.Magnitude > highestVelocity then 
-                highestVelocity = d.Character.PrimaryPart.AssemblyAngularVelocity.Magnitude
-                Highest = d
-            end
-        end
-    end
-end
---pcall(function() print(Highest:GetFullName()) end)
---pcall(function() print(highestVelocity) end)
---print(Vector3.new(0,(math.abs(highestVelocity)*-1)-50,0))
-local FinalVelocity = math.clamp(highestVelocity,50,1500)
-Vehicle.PrimaryPart.AssemblyLinearVelocity = Vector3.new(0,(FinalVelocity*-1)-200,0)
-                --end)
+    )
 until false
